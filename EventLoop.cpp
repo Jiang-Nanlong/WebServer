4067212b6443a6599ac9e1edd03ca4276b223d87
Jiang-Nanlong <aldrichcao0@gmail.com>
2024-11-08 22:14:14 +0800

    add EventLoop module

--- EventLoop.cpp
+++ EventLoop.cpp
@@ -1,12 +1,76 @@
 #include "EventLoop.h"
 
+EventLoop::EventLoop() :
+    isLooping(false),
+    threadId(std::this_thread::get_id()),
+    WakeUpFd(CreateEventFd()),
+    poller(new Poller()),
+    isProcessHandleEvents(false),
+    isProcessTasks(false),
+    WakeUpFdChannel(new Channel(WakeUpFd))
+{
+    WakeUpFdChannel->SetEvents(EPOLLIN | EPOLLET);
+    WakeUpFdChannel->SetReadCallback(bing(&EventLoop::HandleRead, this));
+    poller->UpdateChannel(WakeUpFdChannel, EPOLL_CTL_ADD);
+}
 
+void EventLoop::HandleRead() {
+    uint64_t val = 1;
+    int len = read(WakeUpFd, &val, sizeof(val));
+    if (len < 0) {
+        if (errno == EINTR || errno == EAGAIN) {
+            return;
+        }
+        exit(1);
+    }
+}
+
+void EventLoop::WakeUp() {
+    uint64_t val = 1;
+    int len = write(WakeUpFd, &val, sizeof(val));
+    if (len < 0) {
+        if (errno == EINTR) {
+            return;
+        }
+        exit(1);
+    }
+}
+
+void EventLoop::RunInLoop(const Functor& task) {
+    if (IsInLoop()) return task();
+    return QueueInLoop(task);
+}
+
+void EventLoop::QueueInLoop(const Functor& task) {
+    {
+        lock_guard<mutex> guard(mtx);
+        Tasks.push_back(task);
+    }
+    WakeUp();
+}
+
+void EventLoop::ProcessTasks() {
+    isProcessTasks = true;
+    vector<Functor> functors;
+    {
+        lock_guard<mutex> guard(mtx);
+        functors.swap(Tasks);
+    }
+    for (auto functor : functors)
+        functor();
+    isProcessTasks = false;
+}
 
 void EventLoop::loop() {
-    while (1) {
-        vector<Channel*> res = poller->Poll();
-        for (auto it : res)
+    isLooping = true;
+    while (true) {
+        vector<Channel*> ReadyChannels = poller->Poll();
+        isProcessHandleEvents = true;
+        for (auto& it : ReadyChannels)
             it->HandleEvent();
+        isProcessHandleEvents = false;
 
+        ProcessTasks();
     }
+    isLooping = false;
 }
--- EventLoop.h
+++ EventLoop.h
@@ -2,6 +2,7 @@
 
 #include <thread>
 #include "Poller.h"
+#include "Channel.h"
 #include <functional>
 #include <vector>
 #include <mutex>
@@ -12,14 +13,19 @@ using namespace std;
 
 class EventLoop {
 private:
-    const pid_t threadId;
-    int EventFd;
+    const pid_t threadId;   // 记录创建EventLoop对象的线程号
     shared_ptr<Poller> poller;
+    int WakeUpFd;    // 
+    shared_ptr<Channel> WakeUpFdChannel;
     using Functor = function<void()>;
     vector<Functor> Tasks;
     mutex mtx;
 
-    static int CreateEventFd() {
+    bool isLooping;
+    bool isProcessHandleEvents;  // 是否正在处理poller返回的vector<Channel*>
+    bool isProcessTasks;         // 是否正在处理额外任务
+
+    static int CreateWakeUpFd() {
         int fd = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
         if (fd < 0) {
             exit(1);
@@ -27,8 +33,19 @@ private:
         return fd;
     }
 
+    void ProcessTasks();
 public:
-    EventLoop() :threadId(std::this_thread::get_id()), EventFd(CreateEventFd()), poller(new Poller()) {}
+    EventLoop();
+
+    void HandleRead();
+
+    void WakeUp();
+
+    bool isInLoopThread() const { return threadId == std::this_thread::get_id(); }
+
+    void RunInLoop(const TaskFunc& task);
+
+    void QueueInLoop(const TaskFunc& task);
 
     void loop();
 };
